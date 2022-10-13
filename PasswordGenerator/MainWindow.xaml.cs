21375e98034534870d7e8ff3d00a0d9c105513ae
vivilka <codepen234@gmail.com>
2022-10-13 10:18:55 +0200

    Some code refactor

--- PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -8,56 +8,42 @@ namespace PasswordGenerator
 {
     public sealed partial class MainWindow
     {
-        // Объявление двух field - пароль и длина пароля и одной переменной - максимальной длины.
         private string _password;
         private int _length;
         private const int MaxLength = 100;
+
+        private readonly Password _passwordClass; 
         
-        //Инициализация главного окна.
         public MainWindow()
         {
             InitializeComponent();
+            _passwordClass = new Password(_length);
         }
         
-        //Метод на проверку текста в TextBox длины пароля, 
         private void Length_OnBeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
         {
             args.Cancel = args.NewText.Any(c => !char.IsDigit(c));
         }        
-
-        //Метод, который будет срабатывать при нажаии на кнопку "Сгенерировать пароль".
+        
         private void Generate_click(object sender, RoutedEventArgs e)
         {
-            //Проверка на пустоту поля длины пароля.
-            if (String.IsNullOrEmpty(length.Text) != true)
-            {
-                //Присвоение field длины пароля значения из TextBox путём конвертации String в Int32.
-                _length = Convert.ToInt32(length.Text);
-            }
-            else if (String.IsNullOrEmpty(length.Text) == true)
-            {
-                //Присвоение field длины пароля значения по умолчанию.
+            if (String.IsNullOrEmpty(length.Text))
                 _length = 12;
-            }
 
-            //Проверка на то, что длина пароля не превышает максимальной длины и не меньше нуля.
-            if (_length is <= MaxLength and > 0)
-            {
-                //Присвоению field пароля значение метода GeneratePassword.
-                _password = GeneratePasswordClass.GeneratePassword(_length);
-                //Вывод сгенерированного пароля в TextBlock.
-                pass.Text = _password;
-            }
+            _length = Convert.ToInt32(length.Text);
+
+            if (_length > MaxLength && _length <= 0)
+                return;
+
+            _password = _passwordClass.GeneratePassword();
+            pass.Text = _password;
         }
-
-        //Метод, который будет срабатывать при нажаии на кнопку "Копировать пароль".
+        
         private void ClipSet_click(object sender, RoutedEventArgs e)
         {
-            //Объявление DataPackage, пакета данных.
             var dataPackage = new DataPackage();
-            //Добавление в Datapackage сгенерированного пароля.
             dataPackage.SetText(_password);
-            //Установка пакета данных в буфер обмена.
+            
             Clipboard.SetContent(dataPackage);
         }
     }
---
+++ PasswordGenerator/Password.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    public class Password
+    {
+        private static readonly string[] Symbols = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "[", "]", ";", "'", ".", "/", "{", "}", ":", "?", "<", ">", "|", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "№", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
+
+        private readonly Random _random;
+        private readonly int _passwordLength;
+
+        public Password(int passwordLength)
+        {
+            _passwordLength = passwordLength;
+
+            _random = new Random(); 
+        }
+
+        public String GeneratePassword()
+        {
+            string outputPassword = GetPassword();
+            return outputPassword;
+        }
+
+        private string GetPassword()
+        {
+            string password = ""; 
+            
+            for (int i = 0; i < _passwordLength; i++) 
+                password += Symbols[_random.Next(Symbols.Length)];
+
+            return password;
+        }
+    }
+}
